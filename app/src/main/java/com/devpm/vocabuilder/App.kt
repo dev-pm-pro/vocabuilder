24b61c0f972ee8213421d0010b42ad5351afff40
viktor <sterilework@gmail.com>
2025-09-06 15:12:51 +0300

    feat: add main app class

---
+++ app/src/main/java/com/devpm/vocabuilder/App.kt
@@ -0,0 +1,10 @@
+package com.devpm.vocabuilder
+
+import android.app.Application
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Инициализация глобальных компонентов приложения
+    }
+}
