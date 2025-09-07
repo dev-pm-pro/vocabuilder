903faea9b085f8e6890580d57baee55bb188133f
viktor <sterilework@gmail.com>
2025-09-07 18:50:52 +0300

    feat: add db instance on app class

--- app/src/main/java/com/devpm/vocabuilder/App.kt
+++ app/src/main/java/com/devpm/vocabuilder/App.kt
@@ -1,8 +1,16 @@
 package com.devpm.vocabuilder
 
 import android.app.Application
+import androidx.room.Room
+import com.devpm.vocabuilder.data.AppDb
 
 class App: Application() {
+    val db = Room.databaseBuilder(
+        applicationContext,
+        AppDb::class.java,
+        "app_db"
+    ).build()
+
     override fun onCreate() {
         super.onCreate()
         // Инициализация глобальных компонентов приложения
