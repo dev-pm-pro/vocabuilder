d4616d9ad638faebafc7121dce4c38af8eb996cb
viktor <sterilework@gmail.com>
2025-09-08 11:50:17 +0300

    feat: add binding support

--- app/src/main/java/com/devpm/vocabuilder/activities/LoginActivity.kt
+++ app/src/main/java/com/devpm/vocabuilder/activities/LoginActivity.kt
@@ -6,11 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.devpm.vocabuilder.R
+import com.devpm.vocabuilder.databinding.ActivityLoginBinding
+import com.devpm.vocabuilder.databinding.ActivityRegisterBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
