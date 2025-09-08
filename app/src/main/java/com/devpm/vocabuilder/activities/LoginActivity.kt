225b7cad046ac3fff742adb40804aed4abb0cfbe
viktor <sterilework@gmail.com>
2025-09-08 10:42:26 +0300

    feat: add login activity

---
+++ app/src/main/java/com/devpm/vocabuilder/activities/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.devpm.vocabuilder.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.devpm.vocabuilder.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+    }
+}
