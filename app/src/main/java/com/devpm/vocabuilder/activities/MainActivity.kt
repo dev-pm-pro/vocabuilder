614078e5ee76f51444afa915c6dca5299a14f623
viktor <sterilework@gmail.com>
2025-09-10 02:48:43 +0300

    feat: set initial fragment

--- app/src/main/java/com/devpm/vocabuilder/activities/MainActivity.kt
+++ app/src/main/java/com/devpm/vocabuilder/activities/MainActivity.kt
@@ -5,14 +5,23 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.devpm.vocabuilder.R
+import com.devpm.vocabuilder.fragments.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+    private val profile = ProfileFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Устанавливаем начальный фрагмент
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainContent, profile)
+            .commit()
+
         val intent = Intent(this@MainActivity, LoginActivity::class.java)
-        startActivity(intent)
-        finish()
+        //startActivity(intent)
+        //finish()
     }
 }
