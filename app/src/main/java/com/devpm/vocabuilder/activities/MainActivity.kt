13927977fecd247fd4474e6c0aee4551d74a04d5
viktor <sterilework@gmail.com>
2025-09-10 03:52:08 +0300

    feat: handle controls visual state on click

--- app/src/main/java/com/devpm/vocabuilder/activities/MainActivity.kt
+++ app/src/main/java/com/devpm/vocabuilder/activities/MainActivity.kt
@@ -2,8 +2,11 @@ package com.devpm.vocabuilder.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.devpm.vocabuilder.R
 import com.devpm.vocabuilder.databinding.ActivityMainBinding
@@ -21,11 +24,37 @@ class MainActivity : AppCompatActivity() {
     private val settings = SettingsFragment()
     private val stats = StatsFragment()
 
+    // Элементы навигации
+    private val ctrls by lazy {
+        listOf(
+            Pair(binding.decksImg, binding.decksLbl),
+            Pair(binding.profileImg, binding.profileLbl),
+            Pair(binding.settingsImg, binding.settingsLbl),
+            Pair(binding.statsImg, binding.statsLbl)
+        )
+    }
+
     // Функция замены фрагментов
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.mainContent, fragment)
             .commit()
+    }
+
+    // Сброс цвета всех кнопок/иконок в неактивный цвет
+    private fun resetActiveState() {
+        val inactiveColor = ContextCompat.getColor(this, R.color.cyanDarker)
+        ctrls.forEach {
+            it.first.setColorFilter(inactiveColor)
+            it.second.setTextColor(inactiveColor)
+        }
+    }
+
+    // Установка активного цвета
+    private fun setActiveState(ctl: Pair<ImageView, TextView>) {
+        val activeColor = ContextCompat.getColor(this, R.color.magentaDark)
+        ctl.first.setColorFilter(activeColor)
+        ctl.second.setTextColor(activeColor)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,15 +72,23 @@ class MainActivity : AppCompatActivity() {
         // Обработчики нажатий
         binding.decksBtn.setOnClickListener {
             replaceFragment(decks)
+            resetActiveState()
+            setActiveState(ctrls[0])
         }
         binding.profileBtn.setOnClickListener {
             replaceFragment(profile)
+            resetActiveState()
+            setActiveState(ctrls[1])
         }
         binding.settingsBtn.setOnClickListener {
             replaceFragment(settings)
+            resetActiveState()
+            setActiveState(ctrls[2])
         }
         binding.statsBtn.setOnClickListener {
             replaceFragment(stats)
+            resetActiveState()
+            setActiveState(ctrls[3])
         }
 
         val intent = Intent(this@MainActivity, LoginActivity::class.java)
