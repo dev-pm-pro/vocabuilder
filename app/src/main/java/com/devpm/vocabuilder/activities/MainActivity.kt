ff81c7a81a5cf0661a2fb1a2ad22eeb5095ff953
viktor <sterilework@gmail.com>
2025-09-10 03:02:34 +0300

    feat: add fragment switching

--- app/src/main/java/com/devpm/vocabuilder/activities/MainActivity.kt
+++ app/src/main/java/com/devpm/vocabuilder/activities/MainActivity.kt
@@ -2,13 +2,25 @@ package com.devpm.vocabuilder.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.devpm.vocabuilder.R
 import com.devpm.vocabuilder.fragments.ProfileFragment
+import com.devpm.vocabuilder.fragments.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
+    // Объекты фрагментов
     private val profile = ProfileFragment()
+    private val settings = SettingsFragment()
+
+    // Функция замены фрагментов
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainContent, fragment)
+            .commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +31,14 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.mainContent, profile)
             .commit()
+
+        // Обработчики нажатий
+        findViewById<LinearLayout>(R.id.profileBtn).setOnClickListener {
+            replaceFragment(profile)
+        }
+        findViewById<LinearLayout>(R.id.settingsBtn).setOnClickListener {
+            replaceFragment(settings)
+        }
 
         val intent = Intent(this@MainActivity, LoginActivity::class.java)
         //startActivity(intent)
