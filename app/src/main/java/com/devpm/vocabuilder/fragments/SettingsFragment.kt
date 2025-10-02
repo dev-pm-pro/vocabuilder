7679d06d97da9736611bc34b5f020fe1d0e7b6dd
viktor <sterilework@gmail.com>
2025-10-02 13:30:00 +0300

    chore: remove unused dependencies

--- app/src/main/java/com/devpm/vocabuilder/fragments/SettingsFragment.kt
+++ app/src/main/java/com/devpm/vocabuilder/fragments/SettingsFragment.kt
@@ -6,10 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import com.devpm.vocabuilder.App
-import com.devpm.vocabuilder.R
 import com.devpm.vocabuilder.databinding.FragmentSettingsBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
