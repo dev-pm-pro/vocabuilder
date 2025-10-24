b088d435f14fe81b65f6bbd84a0d38954524e071
viktor <sterilework@gmail.com>
2025-10-24 12:06:04 +0300

    feat: add deck entity

---
+++ app/src/main/java/com/devpm/vocabuilder/data/models/Deck.kt
@@ -0,0 +1,33 @@
+package com.devpm.vocabuilder.data.models
+
+import androidx.room.Dao
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Update
+
+@Entity(tableName = "decks")
+data class Deck(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    var title: String,
+    var userId: Int,
+    var created: Long
+) {
+}
+
+@Dao
+interface DeckDao {
+
+    @Insert
+    suspend fun insertDeck(deck: Deck)
+
+    @Query("SELECT * FROM decks WHERE userId = :uid LIMIT 1")
+    suspend fun getDeckByUid(uid: Int): Deck?
+
+    @Query("SELECT * FROM decks WHERE id = :id LIMIT 1")
+    suspend fun getDeckById(id: Int): Deck?
+
+    @Update
+    suspend fun updateDeck(deck: Deck)
+}
